c6b779b0db3f0fa4a495888da54ce8dcc0ee1282
Sebastian Henao L <sebastianhenaoloaiza@gmail.com>
2021-02-17 17:12:20 -0500

    Cambios en tamaño de fuente

--- js/chart.js
+++ js/chart.js
@@ -1,21 +1,38 @@
 // Bar chart
-new Chart(document.getElementById("bar-chart"), {
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
     type: 'horizontalBar',
     data: {
-        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-        datasets: [
-        {
-            label: "Population (millions)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-            data: [2478,5267,734,784,433]
-        }
-    ]
+        labels: ['Servicio', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [100, 100, 100, 50, 80, 30],
+            backgroundColor: [
+                'rgba(25,131,158, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(25,131,158, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
     },
     options: {
-        legend: { display: false },
-        title: {
-        display: true,
-        text: 'Predicted world population (millions) in 2050'
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
         }
     }
 });
